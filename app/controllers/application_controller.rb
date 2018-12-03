14f011614c0e733372d5ab99d1811c1ca9d0b359
karlverdiere <karl.verdiere@hotmail.fr>
2018-12-03 19:12:03 +0100

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_parameters, if: :devise_controller?
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:usename, :email, :password,:password_confirmation)}
+  end
 end
