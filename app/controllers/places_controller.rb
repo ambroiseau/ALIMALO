84a740fa08a02a56e5f0d4ca5ebcd1b2f2933262
stephan-dev <stephan-dev@outlook.fr>
2018-12-05 15:31:38 +0100

    create controller Places pour marqueurs sur la carte, géoloc, géocode

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,10 @@
+class PlacesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'places/index'
+  get 'places/new'
+  get 'places/create'
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
