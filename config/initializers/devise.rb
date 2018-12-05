da64f93680b9601d56f2a34a90bb2e9e2b62f482
Apelerin <arcanynka@gmail.com>
2018-12-05 16:12:38 +0100

    Confirmation mailer with devise and mailjet

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,10 +18,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'karl.verdiere@hotmail.fr'
+  config.mailer_sender = 'Axel.Alim.Allo@gmail.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'WelcomeMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
