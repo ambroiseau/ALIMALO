71ac9adfff7142cda6bf2f8db15b6bcdc5d0576f
karlverdiere <karl.verdiere@hotmail.fr>
2018-12-06 11:48:11 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'places/index'
   get 'places/new'
   get 'places/create'
-  devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
