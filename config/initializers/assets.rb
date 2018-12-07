32571c6aa6e2c07f6f489a6866580cabffd0f636
stephan-dev <stephan-dev@outlook.fr>
2018-12-07 21:36:11 +0100

    add route to javascripts assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'javascripts')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
