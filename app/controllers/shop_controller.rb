7b6fb0baa371e314f2b33c0b6c3bced541112879
stephan-dev <stephan-dev@outlook.fr>
2018-12-10 18:39:11 +0100

    indentation

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,9 +1,9 @@
 class ShopController < ApplicationController
 	def index
-	@shops= Shop.all
+	  @shops= Shop.all
 	end
 	def show
-	@shop = Shop.find(params[:id])
-	@hours = @shop.operating_hours
+	  @shop = Shop.find(params[:id])
+	  @hours = @shop.operating_hours
 	end
 end
