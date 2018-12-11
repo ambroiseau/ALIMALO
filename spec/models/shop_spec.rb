d011e63a4800ece1e37043ba6780c05f81061a95
karlverdiere <karl.verdiere@hotmail.fr>
2018-12-11 12:05:29 +0100

    test rspec

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+    it 'ensure shop longitude  presence' do
+      shop = Shop.new(title: 'titre', adress: '19 rue de bambou', latitude:'0.488646821e2', tobacco:'true', image_url: 'hiz').save
+      expect(shop).to eq(false)
+    end
+    it 'ensure latitude presence' do
+      shop = Shop.new(title: 'name', adress: 'exampleom', longitude: '0.24041588e1', tobacco:'true', image_url: 'hiz').save
+      expect(shop).to eq(false)
+    end
+    it 'ensure title presence' do
+      shop = Shop.new(adress: 'exampleom', longitude: '0.24041588e1', latitude: '0.24041588e1', tobacco:'true', image_url: 'hiz').save
+      expect(shop).to eq(false)
+    end
+    it 'ensure address presence' do
+      shop = Shop.new(title: 'name', latitude: '0.24041588e1', longitude: '0.24041588e1', tobacco:'true', image_url: 'hiz').save
+      expect(shop).to eq(false)
+    end
+    it 'ensure tabacco presence' do
+      shop = Shop.new(title: 'name', adress:'19 rue du mamouth', latitude: '0.24041588e1', longitude: '0.24041588e1', image_url: 'hiz').save
+      expect(shop).to eq(false)
+    end
+    it 'ensure image_url presence' do
+      shop = Shop.new(title: 'name', adress:'19 rue du mamouth', latitude: '0.24041588e1', longitude: '0.24041588e1').save
+      expect(shop).to eq(false)
+    end
+
+  end
 end
