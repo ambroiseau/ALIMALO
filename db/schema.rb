8d93e1110f5f4cb9631c7726cf124677e5776fd5
karlverdiere <karl.verdiere@hotmail.fr>
2018-12-13 16:57:17 +0100

    rspec

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2018_12_13_000538) do
     t.integer "day"
     t.time "open"
     t.time "close"
-    t.boolean "close_soon"
     t.datetime "valid_from"
     t.datetime "valid_through"
     t.string "name_of_day"
@@ -90,6 +89,7 @@ ActiveRecord::Schema.define(version: 2018_12_13_000538) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
