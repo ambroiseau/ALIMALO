0a8fe1ca49b56dfa734ed146863b905970d015da
vicking421 <vicking421@gmail.com>
2018-12-14 22:15:37 +0100

    Back end Dashboard done, need to add function create shop who automaticly create operaint_hours

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,17 +1,46 @@
 class ShopController < ApplicationController
-	def index
-	  @shops= Shop.all
+before_action :all_shops, only: [:index, :create]
+	respond_to :html, :js
+	def new
+		@shop = Shop.new
+	end
+	def create
+		@shop = Shop.create(shop_params)
+		if @shop.save
+			redirect_to shop_path(@shop.id)
+		else
+			render :action => 'new'
+		end
 	end
 	
   def show
 	  @shop = Shop.find(params[:id])
 	  @hours = @shop.operating_hours
 	end
-
+	def edit
+    @shop = Shop.find(params[:id])
+	end
   def update
     @shop = Shop.find(params[:id])
-    puts "/////////////#{@shop.id}////////////////////#{params[:shop][:frontpicture]}///////////////#{params}///////////////////////////////////////////////"
     @shop.frontpicture.attach(params[:shop][:frontpicture])
     redirect_to "/shop/#{params[:id]}"
+		 if  @shop.update_attributes(shop_params)
+    else
+      render :action => :edit
+    end
   end
+
+	def destroy
+    @shop = Shop.find params[:id]
+    @shop.destroy
+    redirect_to "/shop"
+  end
+	private
+	def all_shops
+		@shops = Shop.all
+	end
+
+	def shop_params
+		params.require(:shop).permit(:title, :adress, :tobacco, :user_id)
+	end
 end
