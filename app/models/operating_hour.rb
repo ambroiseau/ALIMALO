69487880e2824ea4784f8abeeb7d21e3089ccc0d
des_esseintes <ambroise.victorpujebet@gmail.com>
2018-12-16 15:26:43 +0100

    nouveau seed

--- app/models/operating_hour.rb
+++ app/models/operating_hour.rb
@@ -1,5 +1,5 @@
 class OperatingHour < ApplicationRecord
 	belongs_to :shop
 	validates_presence_of :day, :shop_id
-	validates_inclusion_of :day, :in => 1..7
+	validates_inclusion_of :day, :in => 0..6
 end
